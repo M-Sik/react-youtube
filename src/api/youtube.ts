8c5764c8f316dfba0d3e753fa8e521f56b0851eb
김명식 <rlaaudtlr233@naver.com>
2023-06-29 13:54:57 +0900

    [feat] 실제 유튜브 api를 사용할 프로젝트 내부 api 구현

--- src/api/youtube.ts
+++ src/api/youtube.ts
@@ -1,5 +1,45 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+import { VideoItem } from '../types/VideoType';
 
-export async function search(keyword: string) {
-  return axios.get(`/videos/${keyword ? 'search' : 'popular'}.json`).then((res) => res.data.items);
+export default class Youtube {
+  httpClient: any;
+  constructor() {
+    this.httpClient = axios.create({
+      baseURL: 'https://www.googleapis.com/youtube/v3',
+      params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
+    });
+  }
+
+  async search(keyword: string) {
+    //  # 표시는 js에서 private 함수를 뜻하며 클래스 내부에서만 사용할 수 있음
+    return keyword ? this.#searchByKeyword(keyword) : this.#mostPopular();
+  }
+
+  async #searchByKeyword(keyword: string) {
+    return this.httpClient
+      .get('/search', {
+        params: {
+          part: 'snippet',
+          maxReulsts: 25,
+          type: 'video',
+          q: keyword,
+        },
+      })
+      .then((res: AxiosResponse<any, any>) => res.data.items)
+      .then((items: VideoItem[]) =>
+        items.map((item: VideoItem) => ({ ...item, id: item.id.videoId })),
+      );
+  }
+
+  async #mostPopular() {
+    return this.httpClient
+      .get('/videos', {
+        params: {
+          part: 'snippet',
+          maxReulsts: 25,
+          type: 'mostPopular',
+        },
+      })
+      .then((res: AxiosResponse<any, any>) => res.data.items);
+  }
 }
